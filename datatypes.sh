28aeecd2a12f86b1df1be7ccf851d5c82e6bd9ac
Anand <kallaanand0@gmail.com>
2026-01-09 19:47:42 +0530

    somwm

--- datatypes.sh
+++ datatypes.sh
@@ -3,3 +3,11 @@
 SURYA=("Used primarily in professional settings like health, education, and social care, this model focuses on building connection and guiding interactions collaboratively" "This method is designed to help keep communication concise and engaging, preventing rambling by providing a clear structure" "The final phase involves collaborative decision-making to agree on the next steps and potential longer-term support, ensuring the action plan reflects what is truly important to the individua")
 
 echo "${SURYA[2]}"
+
+echo "pls enter the number"
+read number
+if [ ($number % 2) -ne 0 ]; then
+    echo "given $number is odd"
+else
+    echo "given $number is even"
+fi
