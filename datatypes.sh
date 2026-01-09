c889bef6cd274c4a63c9f3c36b90028142eb812b
Anand <kallaanand0@gmail.com>
2026-01-09 19:23:40 +0530

    somwm

--- datatypes.sh
+++ datatypes.sh
@@ -2,4 +2,4 @@
 
 SURYA=("surya is good boy" "surya is handsome person" "surya is always mine about" "surya is great movie actor")
 
-echo "about surya : ${SURYA[1]}
+echo "about surya : ${SURYA[1]}"
