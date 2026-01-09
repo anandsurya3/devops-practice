0862181584736f1cc32c6d1eab051409f2719777
Anand <kallaanand0@gmail.com>
2026-01-09 19:36:11 +0530

    somwm

--- datatypes.sh
+++ datatypes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SURYA=("Connecting The first phase involves actively listening to understand the persons situation and build a relationship ensuring an adulttoadult interaction" "Sharing Information The second phase focuses on working through immediate crises or sharing necessary information potentially developing an emergency plan if needed" "Agreeing on Next Steps The final phase involves collaborative decision making to agree on the next steps and potential longerterm support ensuring the action plan reflects what is truly important to the individua")
+SURYA=("wwwmwmwnwjwjwwjjjjdf" "dvhbvjyvgsfyvrnkvhvfuiv fhuivhfkmvhiu huvhbgfhvfgukhuvhfviuf" "bhrubsuynvkhrunrhikvmhrvnkihrvuigh ybhgvgkuvhfvg")
 
 echo "${SURYA[1]}
