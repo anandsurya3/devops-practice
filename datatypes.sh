7298367e408719d48c1ac7ccc2c46832973c505d
Anand <kallaanand0@gmail.com>
2026-01-09 19:42:02 +0530

    somwm

--- datatypes.sh
+++ datatypes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SURYA=("surys is good" "andnd is fan of surys" "arjunwhat the fuck and somthing")
+SURYA=("Used primarily in professional settings like health, education, and social care, this model focuses on building connection and guiding interactions collaboratively" "This method is designed to help keep communication concise and engaging, preventing rambling by providing a clear structure" "The final phase involves collaborative decision-making to agree on the next steps and potential longer-term support, ensuring the action plan reflects what is truly important to the individua")
 
 echo "${SURYA[2]}"
