e58fffa07ad3f163bc8cf20ede9f9127fb9c725c
Anand <kallaanand0@gmail.com>
2026-01-11 11:02:52 +0530

    somemessage

--- 01-install.sh
+++ 01-install.sh
@@ -5,7 +5,7 @@ if [ $userid -ne 0 ]; then
     echo -e "\e[31m ERROR please take root access \e[0m"
     exit 1
 fi
-function(){
+validate(){
     if [ $1 -ne 0 ]; then
         echo "installing $2 is failed"
         exit 1
@@ -16,14 +16,14 @@ function(){
 dnf list installed mysql
 if [ $? -ne 0 ]; then 
     dnf install mysql -y
-    function $? "mysql"
+    validate $? "mysql"
 else
     echo " mysql is already installed....SKIPPING"
 fi
 dnf list installed nginx
 if [ $? -ne 0 ]; then
     dnf install nginx -y
-    function $? "nginx"
+    validate $? "nginx"
 else
     echo "nginx is already installed....SKIPPING"
 fi
