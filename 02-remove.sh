417e2aa533f55cc221eb113a5952862b101cf079
Anand <kallaanand0@gmail.com>
2026-01-11 14:50:34 +0530

    somemessage

--- 02-remove.sh
+++ 02-remove.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 userid=$(id -u)
-echo $userid
 if [ $userid -ne 0 ]; then
-    echo "ERROR RA BOKKA please take root access"
+    echo "ERROR please take root access"
     exit 1
-dnf remove mysql -y
-
-dnf remove nginx -y
-
-
-echo "please enter the number"
-read number
-if [ $(($number % 2)) -ne 0 ]; then
-    echo "given number is odd"
-else
-    echo "given number is even"
 fi
+dnf remove mysql -y
+dnf remove nginx -y
