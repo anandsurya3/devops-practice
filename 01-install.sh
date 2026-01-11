c758682ca5a0a8f26f2381ad2c46d4dbd97150e8
Anand <kallaanand0@gmail.com>
2026-01-11 11:00:18 +0530

    somemessage

--- 01-install.sh
+++ 01-install.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
-userid=$(id -u)
+userid=(id -u)
 echo $userid
 if [ $userid -ne 0 ]; then
-    echo -e "\e[32m ERROR please take root acces \e[0m"
+    echo -e "\e[31m ERROR please take root access \e[0m"
     exit 1
 fi
-dnf list installed mysql
-if [ $? -eq 0 ]; then
-    dnf install mysql -y
-    if [ $? -eq 0 ]; then
-        echo "installing mysql is failed"
+function(){
+    if [ $1 -ne 0 ]; then
+        echo "installing $2 is failed"
         exit 1
     else
-        echo "installing mysql is success"
-    fi
+        echo "installing $2 is success"
+    fi 
+}
+dnf list installed mysql
+if [ $? -ne 0 ]; then 
+    dnf install mysql -y
+    function $? "mysql"
 else
-    echo -e "mysql already installed....\e[33m SKIPPING \e[0m"
+    echo " mysql is already installed....SKIPPING"
 fi
 dnf list installed nginx
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install nginx -y
-    if [ $? -eq 0 ]; then
-        echo "installing mysql is failed"
-        exit 1
-    else
-        echo "insatlling nginx is success"
-    fi
+    function $? "nginx"
 else
-    echo -e "nginx already installed....\e[33m SKIPPING \e[0m"
+    echo "nginx is already installed....SKIPPING"
 fi
-  
-  echo $0
