52ab2da3786b19438ebb964190185f1c9acb0c27
Anand <kallaanand0@gmail.com>
2026-01-13 23:05:40 +0530

    somemessage

--- 02-remove.sh
+++ 02-remove.sh
@@ -4,7 +4,10 @@ if [ $userid -ne 0 ]; then
     echo "ERROR please take root access"
     exit 1
 fi
-
+log_folder="/var/log/deleted"
+file_name=$( echo $0 | cut -d "." -f1 )
+log_file=$log_folder/$file_name
+mkdir -p $log_folder
 funky(){
     if [ $1 -ne 0 ]; then
         echo "deleting $2 is failed"
@@ -13,18 +16,17 @@ funky(){
         echo "deleting $2 is success"
     fi
 }
-rpm -q mysql
+rpm -q mysql &>>$log_file
 if [ $? -eq 0 ]; then
-    dnf remove mysql
+    dnf remove mysql &>>$log_file
     funky $? "mysql"
 else
     echo "mysql package is already deleted"
 fi 
 
-rpm -q nginx
+rpm -q nginx &>>$log_file
 if [ $? -eq 0 ]; then
-    dnf remove nginx
-    funky $? "nginx"
+    dnf remove nginx &>>$log_file
 else
     echo "nginx package is already deleted"
 fi
