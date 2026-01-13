54584e39c266b31d0e7603e940fb02c35682fb89
Anand <kallaanand0@gmail.com>
2026-01-13 22:53:39 +0530

    somemessage

--- 02-remove.sh
+++ 02-remove.sh
@@ -4,10 +4,7 @@ if [ $userid -ne 0 ]; then
     echo "ERROR please take root access"
     exit 1
 fi
-log_folder="/var/log/deletes"
-script_name=$( echo $0 | cut -d "." -f1 )
-log_file=$log_folder/$script_name
-mkdir -p $log_folder 
+
 funky(){
     if [ $1 -ne 0 ]; then
         echo "deleting $2 is failed"
@@ -17,10 +14,10 @@ funky(){
     fi
 }
 
-    dnf remove mysql &>>$log_file
+    dnf remove mysql
     funky $? "mysql"
 
 
 
-    dnf remove nginx &>>$log_file
+    dnf remove nginx
     funky $? "nginx"
