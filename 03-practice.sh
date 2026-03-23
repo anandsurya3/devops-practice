f0b2c79bc1713928b38ea1dec22403b7df1e803d
Anand <kallaanand0@gmail.com>
2026-03-23 07:24:38 +0530

    somemessage

--- 03-practice.sh
+++ 03-practice.sh
@@ -1,6 +1,6 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-04ed9a90b34bd15be"
-for $instances in $@
+for $instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
     if [ $instances != frontend ]; then
@@ -9,4 +9,4 @@ do
         ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
     fi
 done
-echo "$instances :: $ip"
+echo "$instance:$ip"
