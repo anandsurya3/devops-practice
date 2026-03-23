34644af2afc359579daee5022a081a7857ac0a8f
Anand <kallaanand0@gmail.com>
2026-03-23 07:22:05 +0530

    somemessage

--- 03-practice.sh
+++ 03-practice.sh
@@ -1,5 +1,5 @@
-AMI_ID=$(ami-0220d79f3f480ecf5)
-SG_ID=$(sg-04ed9a90b34bd15be)
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-04ed9a90b34bd15be"
 for $instances in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
