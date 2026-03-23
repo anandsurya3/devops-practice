38622f476009520998599190984aece8142f21c7
Anand <kallaanand0@gmail.com>
2026-03-23 07:32:38 +0530

    somemessage

--- 03-practice.sh
+++ 03-practice.sh
@@ -2,11 +2,11 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-04ed9a90b34bd15be"
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
-    if [ $instances != frontend ]; then
-        ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-    else
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    if [ $instances != "frontend" ]; then
         ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+    else
+        ip=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
     fi
 done
 echo "$instance: $ip"
