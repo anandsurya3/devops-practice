76dea508a5adc0f1d9bc94176b35c9bafc0f0755
Anand <kallaanand0@gmail.com>
2026-03-28 08:58:33 +0530

    some

--- shell-roboshop/mongo.sh
+++ shell-roboshop/mongo.sh
@@ -1,26 +1,27 @@
 userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+W="\3[0m"
 if [ $userid -ne 0 ]; then
     echo"ERROR...take root access"
     exit 1
 fi
-R="\e[31m"
-G="\e[32m"
-W="\3[0m"
+
 logs_folder="/var/log/shell-roboshop"
-script_name=$( echo $0 | cut -d ".' -f1 )
+script_name=$( echo $0 | cut -d "." -f1 )
 log_file="$logs_folder/$script_name.log"
 mkdir -p $logs_folder
 validate(){
-if [ $1 -ne 0 ]; then
-    echo -e "$2...$R FAIL $W"
-else
-    echo -e "$2...$G SUCCESS $W"
-fi
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R FAIL $W" | tee -a $log_file
+    else
+        echo -e "$2...$G SUCCESS $W" | tee -a $log_file
+    fi
 }
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongo-org -y
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+dnf install mongo-org -y &>>$logfile
 validate $? "installing mongodb"
-systemctl enable mongod
+systemctl enable mongod &>>$logfile
 validate $? "enable mongodb"
 systemctl start mongod
 validate $? "start mongodb"
