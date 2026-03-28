4923fdbb034422797048c86742625ffd0f584e7c
Anand <kallaanand0@gmail.com>
2026-03-28 17:30:31 +0530

    some

--- shell-roboshop/redis.sh
+++ shell-roboshop/redis.sh
@@ -1,52 +1,47 @@
 #!/bin/bash
 
-userid=$(id -u)
-if [ $userid -ne 0 ]; then
-    echo "ERROR...దయచేసి root access తో రన్ చేయండి (Use sudo)"
-    exit 1
-fi
-
-start_time=$(date +%s)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-W="\e[0m"
+Y="\e[33m"
+N="\e[0m"
 
-log_folder="/var/log/shell-roboshop"
-# script_name లో పాత్ లేకుండా కేవలం పేరు మాత్రమే వచ్చేలా మార్చాను
-script_name=$(basename "$0" | cut -d "." -f1)
-log_file="$log_folder/$script_name.log"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-# ఫోల్డర్ క్రియేషన్
-mkdir -p $log_folder
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
 
-validate(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$2...$R విఫలమైంది (Failed) $W" | tee -a $log_file
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-        echo -e "$2...$G విజయవంతమైంది (Success) $W" | tee -a $log_file
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-# Redis పాత మాడ్యూల్స్ క్లియర్ చేయడం
-dnf module disable redis -y &>>$log_file
-validate $? "Redis మాడ్యూల్ డిసేబుల్ చేయడం"
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y  &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
 
-dnf module enable redis:7 -y &>>$log_file
-validate $? "Redis 7 మాడ్యూల్ ఎనేబుల్ చేయడం"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote connections to Redis"
 
-dnf install redis -y &>>$log_file
-validate $? "Redis ఇన్‌స్టాల్ చేయడం"
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
 
-# IP Address మార్పు మరియు Protected Mode ఆపడం
-# ఇక్కడ sed కమాండ్ ని విడివిడిగా రాస్తే క్లారిటీ ఉంటుంది
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>>$log_file
-sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf &>>$log_file
-validate $? "రిమోట్ కనెక్షన్స్ అనుమతించడం"
-
-systemctl enable redis &>>$log_file
-validate $? "Redis ఎనేబుల్ చేయడం"
-
-systemctl start redis &>>$log_file
-validate $? "Redis స్టార్ట్ చేయడం"
-
-echo -e "$G స్క్రిప్ట్ పూర్తయింది! $W"
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
