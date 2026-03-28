5d27cc9fac09d8b8e06b77639af834d69494fa34
Anand <kallaanand0@gmail.com>
2026-03-28 14:57:38 +0530

    some

---
+++ shell-roboshop/redis.sh
@@ -0,0 +1,34 @@
+userid=$(id -u)
+if [ $userid -ne 0 ]; then
+    echo "ERROR...pls take root access"
+    exit 1
+fi
+start_time=$(date +%s)
+G="\e[31m"
+R="\e[32m"
+W="\e[0m"
+log_folder="/var/log/shell-roboshop"
+script_name=$( echo $0 | cut -d "." -f1 )
+log_file="$log_folder/$script_name.log"
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R fialed $W"
+    else
+        echo -e "$2...$G success $W"
+    fi
+}
+dnf module disable redis -y
+validate $? "disabling redis"
+dnf module enable redis:7 -y
+validate $? "enabling redis"
+dnf install redis -y
+validate $? "installing redis"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c proctect-mode no' /etc/redis/redis.conf
+validate $? "allowing connections and changed protected mode"
+systemctl enable redis
+validate $? "enabling redis"
+systemctl start redis
+validate $? "starting redis"
+end_time=$(date +%s)
+total_time=$(( $end_time - $start_time ))
+echo "script executed total time :: $total_time
